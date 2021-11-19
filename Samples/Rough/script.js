44c139bfa7d73e3f45b5109211df000afdd6c35c
Vishnu Prasad M <vpm735@gmail.com>
2021-11-19 16:46:32 +0530

    dom

--- Samples/Rough/script.js
+++ Samples/Rough/script.js
@@ -1,7 +1,44 @@
-alert("Demo alert");
+
 console.error("Demo error");
 
 let number = 83678;
 
     document.write("<h1>Hello Js</h1>")
     document.write("<h1>Another</h1>")
+    document.write("<h1>typeof number</h1>")
+var v = document.querySelectorAll("h1")
+
+    function show(){
+        for (let i=0;i<5;i++){
+            console.log(i);
+        }
+let i = 0;
+        while (i<=5){
+            console.log(i);
+            i++;
+        }
+        do{
+            console.log(i);
+            i++;
+        }
+        while (i<=5);
+    }
+
+    function print(){
+        
+        {var a=20;}
+       
+        let b = a++;
+        console.log (a);
+        let c = ++a;
+       
+        console.log (b);
+        console.log (c);
+        console.log(a>b);
+
+        let f  = 1001;
+    let g =1010;
+    console.log (f&g);
+    }
+    
+    print();
