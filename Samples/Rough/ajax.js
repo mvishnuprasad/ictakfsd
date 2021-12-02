964c5976ac62bf97146c503563fc13a975fa1dcd
Vishnu Prasad M <vpm735@gmail.com>
2021-12-02 13:27:41 +0530

    ajax

---
+++ Samples/Rough/ajax.js
@@ -0,0 +1,18 @@
+function ajax(){
+
+
+    // creating an XHR object with
+    var xhttp = new XMLHttpRequest();
+    // Event Listner
+    
+    xhttp.onreadystatechange = function(){
+        //check conditions of
+        if(this.readyState ==4&&this.status == 200){
+            document.getElementById("demo").innerHTML= this.responseText;
+    
+        }
+    }
+    //initiate
+    xhttp.open("GET", "ajax.txt",true);
+    xhttp.send();
+    }
