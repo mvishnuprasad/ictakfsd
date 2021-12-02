a64b1f4db36e2c42fd076fa56278080fbbf81879
Vishnu Prasad M <vpm735@gmail.com>
2021-12-02 23:07:09 +0530

    a

--- Samples/Rough/es5.js
+++ Samples/Rough/es5.js
@@ -1,9 +1,22 @@
 // "use strict"
 // x=22;
 
-function press(){
-    "use strict";
-    y=33;
-}
+// function press(){
+//     "use strict";
+//     y=33;
+// }
 
-press();
+// press();
+
+"use strict";
+var y = 33;
+// delete y;
+eval("var y = 3")
+console.log(y)
+var str = "This is a string   message  ";
+document.write("<br>"+str.length);
+document.write("<br>"+str.indexOf("message"));
+document.write("<br>"+str[2]);
+document.write("<br>"+str.replace(" is", "  IS"));
+document.write("<br>"+str.trim());
+document.write("<br>"+str.split(""));
