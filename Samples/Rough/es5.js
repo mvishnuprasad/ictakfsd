9231f7020954df6ddc870d88a8197a08fa2641ae
Vishnu Prasad M <vpm735@gmail.com>
2021-12-03 11:57:34 +0530

    callback

--- Samples/Rough/es5.js
+++ Samples/Rough/es5.js
@@ -21,4 +21,28 @@ document.write("<br>"+str.replace(" is", "  IS"));
 document.write("<br>"+str.trim());
 document.write("<br>"+str.split(" ,"));
 document.write("<br>"+str.slice(4,9));
-document.write("<br>"+str.substring(1,2))
+document.write("<br>"+str.substring(1,2));
+
+// var fruits = ["Aa","va","ca","AAAA","EDE","iedj"];
+// document.write("<br>"+Array.isArray(fruits));
+// fruits.forEach(func);
+
+// function func(value){
+//     document.write("<br>"+value);
+// }
+// fruits.filter(func1);
+// function func1(value1){
+//     return value1.startsWith("c");
+// }
+// document.write("<br> "+fruits.filter(func1));
+
+function argFunction(val){
+    document.write(val);
+}
+
+function callbackFunction (var1 , Callback){
+    var1+="a";
+    Callback(var1);
+}
+
+ callbackFunction("Zebr" , argFunction);
